a56c83185c7c7d685d731d2c2c3fb8fb82e142ac
justnoxx <justnoxx@gmail.com>
2014-12-21 22:47:04 +0200

    Connpool rc

---
+++ lib/AnyEvent/ConnPool.pm
@@ -0,0 +1,302 @@
+=head1 NAME
+
+AnyEvent::ConnPool
+
+=head1 DESCRIPTION
+
+Simple connections pool designed for asynchronous connections
+
+=head1 METHODS
+
+=over
+
+=cut
+
+package AnyEvent::ConnPool;
+use strict;
+use warnings;
+
+use Data::Dumper;
+use AnyEvent;
+use Carp;
+
+our $VERSION = 0.03;
+
+my $PID;
+
+BEGIN {
+    $PID = $$;
+}
+
+
+=item B<new>
+
+Returns new pool object
+
+    AnyEvent::ConnPool->new(
+        constructor =>  sub {
+            return generate_connection();
+        },
+        check       =>  {
+            cb          =>  sub {
+                my $connection = shift;
+                ...
+                if ($connection->ping()) {
+                    return 1;
+                }
+                return 0;
+            },
+            interval    =>  10,
+        },
+        size        =>  5,
+        init        =>  1,
+    );
+
+
+B<constructor> subroutine, which generates connection for pool.
+
+B<check> pingers, allows to specify methods and interval for connection state validation. If false, pool will try to reopen connection.
+
+B<size> how many connections should be created on pool initialization.
+
+B<init> initialize connections on pool construction.
+
+=cut
+
+sub new {
+    my ($class, %opts) = @_;
+
+    my $self = {
+        constructor     =>  sub {1;},
+        _payload        =>  [],
+        index           =>  0,
+        init            =>  0,
+        count           =>  0,
+    };
+
+    if (!$opts{constructor}) {
+        croak "Missing mandatory param constructor.";
+    }
+    if (ref $opts{constructor} ne 'CODE') {
+        croak "Constructor should be a code reference.";
+    }
+    
+    $self->{constructor} = $opts{constructor};
+
+    if ($opts{check}) {
+        if (ref $opts{check} ne 'HASH') {
+            croak "Check param should be a hash reference.";
+        }
+        if (!$opts{check}->{cb}) {
+            croak 'Missing cb param.';
+        }
+        
+        if (ref $opts{check}->{cb} ne 'CODE') {
+            croak 'Cb param should be a code reference.';
+        }
+        if (!$opts{check}->{interval}) {
+            croak 'Missing interval param.';
+        }
+        # TODO: Add interval parameter validation
+        
+        $self->{check} = $opts{check};
+    }
+
+    if ($opts{size}) {
+        #TODO: add validation for size
+
+        $self->{size} = $opts{size};
+    }
+
+    bless $self, $class;
+
+    if ($opts{init}) {
+        $self->init();
+    }
+
+    return $self;
+}
+
+
+=item B<init>
+Initializes pool.
+=cut
+
+sub init {
+    my ($self, $conn_count) = @_;
+    
+    if ($self->{init}) {
+        croak "Can't initialize already initilized pool.";
+    }
+    
+    $conn_count ||= delete $self->{size};
+
+    unless ($conn_count) {
+        croak "Can't initilize empty pool";
+    }
+    
+    for (1 .. $conn_count) {
+        $self->add();
+    }
+    
+    if ($self->{check}) {
+        my $guard; $guard = AnyEvent->timer (
+            after       =>  $self->{check}->{interval},
+            interval    =>  $self->{check}->{interval},
+            cb          =>  sub {
+                for (my $i = 0; $i < $self->{count}; $i++) {
+                    my $conn = $self->{_payload}->[$i];
+                    $self->{check}->{cb}->($conn);
+                }
+            },
+        );
+    }
+
+    $self->{init} = 1;
+    return 1;
+}
+
+
+sub add {
+    my ($self, $count) = @_;
+    
+    my $conn = $self->{constructor}->();
+    my $unit = AnyEvent::ConnPool::Unit->new($conn);
+    $self->_add_object_raw($unit);
+
+}
+
+
+sub get {
+    my ($self, $index) = @_;
+    
+    if (defined $index) {
+        return $self->{_payload}->[$index];
+    }
+
+    if ($self->{index} + 1 > $self->{count}) {
+        $self->{index} = 0;
+    }
+
+    my $retval = $self->{_payload}->[$self->{index}];
+    
+    if ($retval->locked()) {
+        $self->{locked}->{$self->{index}} = 1;
+        $retval = $self->get_free_connection($self->{index});
+    }
+    else {
+        delete $self->{locked}->{$self->{index}};
+    }
+
+    if (wantarray) {
+        $self->{index}++;
+        return ($index, $retval);
+    }
+    else {
+        $self->{index}++;
+        return $retval;
+    }
+}
+
+sub get_free_connection {
+    my ($self, $desired_index) = @_;
+    
+    my $retval = undef;
+    my @balanced_array = $self->balance_array($desired_index);
+
+    for my $i (@balanced_array) {
+        my $conn = $self->{_payload}->[$i];
+        unless ($conn->locked) {
+            $retval = $conn;
+            $self->{index} = $i;
+            last;
+        }
+    }
+    return $retval;
+    
+}
+
+
+sub balance_array {
+    my ($self, $index) = @_;
+
+    my $count = $self->{count};
+    $index++;
+    $count--;
+
+    if ($index == 0 || $index >= $count) {
+        return (0 .. $count);
+    }
+
+    return (($index .. $count), 0 .. $index - 1);
+}
+
+
+sub _add_object_raw {
+    my ($self, $object, $position) = @_;
+    
+    if (defined $position) {
+        $self->{_payload}->[$self->{index}] = $object;
+    }
+    else {
+        push @{$self->{_payload}}, $object;
+    }
+
+    $self->{count} = scalar @{$self->{_payload}};
+    return 1;
+}
+
+
+=back
+=cut
+
+1;
+
+package AnyEvent::ConnPool::Unit;
+use strict;
+use warnings;
+
+
+sub new {
+    my ($class, $object) = @_;
+
+    my $unit = {
+        _conn   =>  $object,
+        _locked  =>  0,
+    };
+
+    bless $unit, $class;
+    return $unit;
+}
+
+
+sub conn {
+    my $self = shift;
+    return $self->{_conn};
+}
+
+
+sub lock {
+    my ($self) = @_;
+
+    $self->{_locked} = 1;
+    return 1;
+}
+
+
+sub unlock {
+    my ($self) = @_;
+
+    delete $self->{_locked};
+    return 1;
+}
+
+
+sub locked {
+    my ($self) = @_;
+    
+    return $self->{_locked};
+}
+
+1;
+
---
+++ t/01-import.t
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Test::More tests => 1;
+
+use_ok('AnyEvent::ConnPool') or BAIL_OUT "Can't continue without import";
+
